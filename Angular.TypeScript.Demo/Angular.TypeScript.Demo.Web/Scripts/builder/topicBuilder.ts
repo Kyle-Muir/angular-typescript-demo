6955ebce2edab4e9e5c1ad483641d58e6fa555c9
Kyle-Muir <kyle.matthew.muir@gmail.com>
2015-06-25 07:35:58 +1200

    Added builders to the demo

---
+++ Angular.TypeScript.Demo/Angular.TypeScript.Demo.Web/Scripts/builder/topicBuilder.ts
@@ -0,0 +1,16 @@
+﻿module Builder {
+    export class TopicBuilder {
+        public static buildTopic(topic: Integration.IRedditTopic): Model.Topic {
+            var submitter = new Model.Submitter(topic.Submitter.Id, topic.Submitter.Name);
+            return new Model.Topic(topic.Id, topic.Title, topic.HostUrl, new Date(topic.SubmissionTime), topic.Subreddit, submitter);
+        }
+
+        public static buildTopics(topics: Array<Integration.IRedditTopic>): Array<Model.Topic> {
+            var items = [];
+            _.each(topics, (item: Integration.IRedditTopic) => {
+                items.push(this.buildTopic(item));
+            });
+            return items;
+        }
+    }
+} 
--- Angular.TypeScript.Demo/Angular.TypeScript.Demo.Web/Scripts/topicRepository.ts
+++ Angular.TypeScript.Demo/Angular.TypeScript.Demo.Web/Scripts/topicRepository.ts
@@ -16,19 +16,13 @@ module Services {
             };
             return this.$http.post('http://localhost:52558/api/RedditClone', data).then((resp: Integration.IGenericHttpResponse<Integration.IRedditTopic>) => {
                 var redditTopic = resp.data;
-                var submitter = new Model.Submitter(redditTopic.Submitter.Id, redditTopic.Submitter.Name);
-                return new Model.Topic(redditTopic.Id, redditTopic.Title, redditTopic.HostUrl, new Date(redditTopic.SubmissionTime), redditTopic.Subreddit, submitter);
+                return Builder.TopicBuilder.buildTopic(redditTopic);
             });
         }
 
         public loadTopics = (): ng.IPromise<Array<Model.Topic>> => {
             return this.$http.get('http://localhost:52558/api/RedditClone').then((resp: Integration.IGenericHttpResponse<Array<Integration.IRedditTopic>>) => {
-                var items = [];
-                _.each(resp.data, (item: Integration.IRedditTopic) => {
-                    var submitter = new Model.Submitter(item.Submitter.Id, item.Submitter.Name);
-                    items.push(new Model.Topic(item.Id, item.Title, item.HostUrl, new Date(item.SubmissionTime), item.Subreddit, submitter));
-                });
-                return items;
+                return Builder.TopicBuilder.buildTopics(resp.data);
             });
         }
     }
